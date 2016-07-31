bbfb3c24c08bac24dc7b7da113ecebf9392a6c0b
jkillz2020 <kilgore.josh@gmail.com>
2016-07-31 16:12:21 -0500

    letters stacking every 5th ltr

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-var alpha = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var newString = [];
 
-function alphastacker = for (var i = 0; i < alpha.length; i++) {
-  alpha[i]
+function alphaStacker(){
+  for (var i = 0; i < alpha.length; i++) {
+    if(i%5 === 0) {
+      newString += " " + alpha[i]
+    } else {
+      newString += alpha[i]
+      console.log(newString)
+    }
+  }
 }
+alphaStacker()
+
+
+
+
+
+
